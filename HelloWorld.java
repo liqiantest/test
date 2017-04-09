b1bff8aff629a0d299ce5555c5f266edaf385ad6
SAP HANA Database System Administrator <hxeadm@xiyl50833395a.apj.global.corp.sap>
2017-04-09 10:55:19 +0800

    add key

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 package com.liqian.testxsa;
 
 public class HelloWorld {
+           
+       private String key;       
 
 	public static void main(String[] args){
 		System.out.println("hello world......");
